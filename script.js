8f0527a84d99721d23032f71b9d3f8fa80d7b2ac
Rekkuzan5 <stapp.cameron@gmail.com>
2019-09-22 23:49:22 -0500

    Added order details and fixed logic, completed build.  Could use refining.

--- script.js
+++ script.js
@@ -14,3 +14,32 @@ function listPizza() {
         console.log("<option onclick='listPizza(this)' value='" + i + "'>" + pizzas[i] + "</option>");
     }
 }
+
+function listQuantity() {
+    for (var i = 1; i <= 10; i++)
+    {
+        document.write("<option value='" + i + "'>" + i + "</option>");
+
+    }
+}
+
+function orderNow() {
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("phone").value;
+    var type = document.getElementById("pizzaType");
+    var typeText = type.options[type.selectedIndex].text;
+    var pizzaQuantity = document.getElementById("pizzaQuant").value;
+
+    var pizzaPrice = 8.99;
+    var tax = .076;
+
+    var price = (pizzaQuantity * pizzaPrice);
+    var calculatedTax = price * tax;
+    var totalPrice = price + calculatedTax;
+
+    var string = "Customer Name: " + name + "<br/>" + "Customer Phone: " + phone +
+                    "<br/>" + "Number of Pizzas: " + pizzaQuantity + "<br/>" + "Type of Pizza: " + typeText + "<br/>" +
+                    "Price: $" + totalPrice.toFixed(2);
+    
+    document.getElementById("summary").innerHTML = string;
+}
